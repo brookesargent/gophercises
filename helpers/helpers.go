5e2c5e38dc3943a0ff3a2c3c1b7faa73bbcd4b15
brookesargent <brookesargent01@gmail.com>
2020-11-03 11:37:15 -0500

    working solution for exercise 2

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,15 +1,15 @@
 package helpers
 
 import (
+	"gopkg.in/yaml.v2"
 	"net/http"
 )
 
-// MapHandler will return an http.HandlerFunc (which also
-// implements http.Handler) that will attempt to map any
-// paths (keys in the map) to their corresponding URL (values
-// that each key in the map points to, in string format).
-// If the path is not provided in the map, then the fallback
-// http.Handler will be called instead.
+type PathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
+
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
@@ -21,25 +21,23 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
-// YAMLHandler will parse the provided YAML and then return
-// an http.HandlerFunc (which also implements http.Handler)
-// that will attempt to map any paths to their corresponding
-// URL. If the path is not provided in the YAML, then the
-// fallback http.Handler will be called instead.
-//
-// YAML is expected to be in the format:
-//
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
-//
-// The only errors that can be returned all related to having
-// invalid YAML data.
-//
-// See MapHandler to create a similar http.HandlerFunc via
-// a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		
-		fallback.ServeHTTP(w, r)
-	}, nil
+	pathUrls, err := parseYAML(yml)
+	if err != nil {
+		return nil, err
+	}
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+func parseYAML(yml []byte) ([]PathUrl, error) {
+	var pathUrls []PathUrl
+	err := yaml.Unmarshal(yml, &pathUrls)
+	if err != nil {
+		return pathUrls, err
+	}
+	return pathUrls, nil
 }
